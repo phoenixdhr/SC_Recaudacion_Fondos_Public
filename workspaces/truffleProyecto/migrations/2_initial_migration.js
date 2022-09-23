1a4c4aead3f70b1b4ea11da454a56f7df4d6d33d
danyr <phoenixdhr@gmail.com>
2022-09-23 12:23:59 -0500

    se elimino el Proyeto de Truffle, tenia fallas

--- workspaces/truffleProyecto/migrations/2_initial_migration.js
+++
@@ -1,6 +0,0 @@
-//2_initial_migration.js
-const CrowdFunding = artifacts.require("CrowdFunding"); //Instancia de nuestro contrato CrowdFunding.sol
-
-module.exports = function (deployer) {
-  deployer.deploy(CrowdFunding); //Este script hace deploy de nuestro contrato a la blockchain
-};
